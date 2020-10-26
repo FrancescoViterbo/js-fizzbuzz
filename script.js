bf6fc5ac17444816d95bdfcccc47f38d1d11e44e
FrancescoViterbo <franc.viterbo@gmail.com>
2020-10-26 13:39:47 +0100

    Programma funzionante, ma presenta saltuariamente un bug che blocca la pagina

---
+++ script.js
@@ -0,0 +1,29 @@
+const btnGenera = document.getElementById("btn-genera");
+const numeroPartenza = document.getElementById("numero-partenza");
+const listaNumeri = document.getElementById("lista-numeri");
+
+btnGenera.addEventListener("click", generaNumeri);
+
+function generaNumeri (e) {
+    e.preventDefault();
+
+    let numeroDiPartenza = numeroPartenza.value;
+    for (i = numeroDiPartenza; i < numeroDiPartenza + 100; i++) {
+        var listItem = document.createElement("li");
+        // if (i = 0) {
+        //     listItem.innerHTML = "0";
+        // } 
+        if (i === 0) {
+            listItem.innerHTML = "0";
+        } else if ((i % 3) === 0 && (i % 5) === 0) {
+            listItem.innerHTML = "FizzBuzz";
+        } else if ((i % 3) === 0) {
+            listItem.innerHTML = "Fizz";
+        } else if ((i % 5) === 0) {
+            listItem.innerHTML = "Buzz";
+        } else {
+            listItem.innerHTML = i;
+        }
+        listaNumeri.appendChild(listItem);
+    }
+}
